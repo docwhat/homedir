e042ed4ba73e8793f9581aad97a87a756c74cba6
Christian Höltje <docwhat@gerf.org>
2012-02-18 23:00:46 -0500

    Fixed errors with getcwd in rspec
    The formatters didn't like my usage of around(), so
    I switched to before()/after().

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,21 @@
+require 'homedir/hacks'
+require 'fileutils'
 require 'tmpdir'
 require 'factory_girl'
 require 'factories'
-
-def within_a_tmpdir
-  Dir.mktmpdir('homedir-spec-') do |dir|
-    old_path = Dir.pwd
-    Dir.chdir(dir)
-    yield
-    Dir.chdir(old_path)
-  end
-end
 
 RSpec.configure do |config|
   # Allows using build(), create(), etc. without the "FactoryGirl." part.
   config.include FactoryGirl::Syntax::Methods
 
-  config.around(:each) do |example|
-    within_a_tmpdir(&example)
+  config.before(:each) do
+    @directory = Dir.mktmpdir('homedir-spec-')
+    @orig_directory = Dir.pwd
+    Dir.chdir(@directory)
+  end
+
+  config.after(:each) do
+    Dir.chdir(@orig_directory)
+    FileUtils.rmtree(@directory)
   end
 end
